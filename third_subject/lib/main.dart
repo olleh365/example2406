3c4f0a6c2d09a3b7d0146c05ef8f2741b394bddd
junwoo.h <junwoo.h@softlunch.com>
2024-06-28 20:48:15 +0900

    flutter dio 라이브러리 사용으로 변경

--- third_subject/lib/main.dart
+++ third_subject/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 
 void main() {
@@ -7,44 +7,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: _WeatherScreen(),
+    return const MaterialApp(
+      home: WeatherScreen(),
     );
   }
 }
 
-class _WeatherScreen extends StatefulWidget {
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({super.key});
   @override
-  _WeatherScreenState createState() => _WeatherScreenState();
+  WeatherScreenState createState() => WeatherScreenState();
 }
 
-class _WeatherScreenState extends State<_WeatherScreen> {
+class WeatherScreenState extends State<WeatherScreen> {
   String weatherData = '';
   bool isLoading = false;
+  final _dio = Dio();
 
   void fetchWeather() async {
     setState(() {
       isLoading = true;
     });
     try {
-      var url = Uri.parse(
-          'https://api.openweathermap.org/data/2.5/weather?lat=37.5511543&lon=126.9727751&appid=fe8f9cb8d0946ab928eea3124790e656');
-      var response = await http.get(url);
+      var response = await _dio.get('https://api.openweathermap.org/data/2.5/weather?lat=37.5511543&lon=126.9727751&appid=fe8f9cb8d0946ab928eea3124790e656');
 
-
-      if (response.statusCode == 200) {
-        setState(() {
-          weatherData = response.body;
-        });
-      } else {
-        setState(() {
-          weatherData = '호출 실패 - Status Code: ${response.statusCode}';
-        });
-      }
+      weatherData = response.data.toString();
     } catch (e) {
       setState(() {
         weatherData = '예외 발생: $e';
@@ -60,7 +51,7 @@ class _WeatherScreenState extends State<_WeatherScreen> {
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(
-          title: const Text('Weather App'),
+          title: const Text('Third Subject App'),
         ),
         body: Center(
           child: Column(
